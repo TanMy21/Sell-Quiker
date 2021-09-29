54ee2f909465106394db7357efa009c6a423a961
Tanmay 1 <tanmay.pvp.17@gmail.com>
2021-09-29 19:01:44 +0530

    List Details

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, SafeAreaView } from "react-native";
-import AppButton from "./app/components/AppButton";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+// import { View, SafeAreaView } from "react-native";
+// import AppButton from "./app/components/AppButton";
+// import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+// import ViewImageScreen from "./app/screens/ViewImageScreen";
 // import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
@@ -10,6 +12,8 @@ export default function App() {
   return (
     // <WelcomeScreen />
     // <ViewImageScreen />
-  );
+    // <Card title="Title" subTitle="sub title" image={require("./app/assets/")}/>
+    <ListingDetailsScreen />
+    );
 }
 
---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+import AppText from "../components/AppText";
+
+import ListItem from "../components/ListItem";
+import colors from "../config/colors";
+
+function ListingDetailsScreen(props) {
+  return (
+    <View>
+      <Image
+        style={styles.image}
+        source={require("../assets/CoffeeTable.jpg")}
+      />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Coffee Table for sale</AppText>
+        <AppText style={styles.price}>Rs.5000</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/person1.png")}
+            title="John Doe"
+            subTitle="5 Listings"
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  detailsContainer: {
+    padding: 20,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 8,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  userContainer: {
+    marginVertical: 40,
+  },
+});
+
+export default ListingDetailsScreen;
